7479cc12b2666cb116ea5b83b6d66adcccb8741f
DerThorsten <derthorstenbeier@gmail.com>
2023-03-11 14:11:08 +0100

    no build system

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -8,7 +8,7 @@
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
 // Option 2
@@ -18,8 +18,8 @@
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
+// import "../../node_modules/bootstrap/js/dist/util.js";
+// import "../../node_modules/bootstrap/js/dist/modal.js";
 
 
 function choose(choices) {
@@ -29,13 +29,15 @@ function choose(choices) {
 
 let genres = null
 
-function pic_genre(genres){
+
+var select = document.getElementById('genreRootSelect');
+
+function pick_genre(genres){
     
-    let current_genres = globalThis.genres
+    let current_genres = genres
     let path = []  
     while(true){
         let genre = choose(Object.keys(current_genres))
-        console.log(`genre: ${genre}`)
         path.push(genre)
         let sub_genre = current_genres[genre]
         if (sub_genre === null){
@@ -51,11 +53,29 @@ function pic_genre(genres){
 function on_generate(){
     console.log("generate")
     var output_area = $('#output_area');
-    output_area.text("bazinga");
+    
+    let root_genre = null;
+    let style_genre = null;
 
-    // pick genre
-    const genre = pic_genre()
-    output_area.text(genre);
+    var root_genre_item = select.options[select.selectedIndex];
+    if(root_genre_item !== undefined){
+        let root_genre_start = root_genre_item.text
+
+        // pick root genre
+        root_genre = [root_genre_start].concat(pick_genre(globalThis.genres[root_genre_start]))
+        // pick style genre
+        style_genre = pick_genre(globalThis.genres)
+
+    }
+    else{
+        // root genre
+        root_genre = pick_genre(globalThis.genres)
+        // pick style genre
+        style_genre = pick_genre(globalThis.genres)
+
+    }
+    output_area.text(`${root_genre} in the style of ${style_genre}`);
+
 };
 
 
@@ -65,10 +85,22 @@ async function main(){
     let json  = await r.json()
     delete json.__meta__
     globalThis.genres = json
+    
 
-    console.log(json)
+
+
+    for (const [key, value] of Object.entries(globalThis.genres)) {
+
+        var opt = document.createElement('option');
+        opt.value = key;
+        opt.innerHTML = key;
+        select.appendChild(opt);
+    }
+
+
 
     document.getElementById('generate_button').onclick = on_generate;
+
 
 }
 
